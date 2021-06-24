b9fcf224d96a1b68414c9a13db9475872afa355b
Jeferson Santos <jefo2k@gmail.com>
2021-06-23 22:14:56 -0300

    first game entities version and tests

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,91 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+class BinTreeNode(
+  val statement: String,
+  val left: BinTreeNode,
+  val right: BinTreeNode
+)
 
-def msg = "I was compiled by Scala 3. :)"
+case class CharacteristicGuess(
+  val characteristic: String,
+  val yes: BinTreeNode, // TODO accept only CharacteristicGuess and AnimalGuess types
+  val no: BinTreeNode   // TODO accept only CharacteristicGuess and AnimalGuess types
+) extends BinTreeNode(
+  statement = characteristic,
+  left = yes,
+  right = no
+)
+
+case class AnimalGuess(
+  val characteristic: String,
+  val yes: AnimalFound = AnimalFound(),
+  val no: AnimalNotFound = AnimalNotFound()
+) extends BinTreeNode(
+  statement = characteristic,
+  left = yes,
+  right = no
+)
+
+case class AnimalFound(
+  val name: String = "You win!",
+  val yes: BinTreeNode = null,
+  val no: BinTreeNode = null
+) extends BinTreeNode(
+  statement = name,
+  left = yes,
+  right = no
+)
+
+case class AnimalNotFound(
+  val message: String = "Animal Not Found!",
+  val yes: BinTreeNode = null,
+  val no: BinTreeNode = null
+) extends BinTreeNode(
+  statement = message,
+  left = yes,
+  right = no
+)
+
+object AnimalGame:
+  def build(): AnimalGame = {
+    val newCharacteristic =
+         CharacteristicGuess(characteristic = "Does the animal meow?",
+         yes = AnimalGuess(characteristic =  "Is it a cat?"),
+         no = AnimalGuess(characteristic = "Is the animal a dog?!?")
+      )
+    new AnimalGame(newCharacteristic)
+  }
+
+class AnimalGame(
+                val root : CharacteristicGuess
+                ):
+  def isReady(): Boolean = {
+    true
+  }
+
+//@main def hello: Unit = {
+//  println("Animal Game!")
+
+//  val dog = AnimalGuess("Is the animal a dog?!?")
+//  println(s"$dog")
+//
+//  val yes = dog.yes match {
+//    case AnimalFound(name, yes, no) => name
+//    case _ => println("Error!")
+//  }
+//
+//  val no = dog.no match {
+//    case AnimalNotFound(message, yes, no) => message
+//    case _ => println("Error!")
+//  }
+//
+//  println(s"yes $yes, no $no")
+
+
+
+ // val newCharacteristic =
+//     CharacteristicGuess(characteristic = "Does the animal meow?",
+//     yes = AnimalGuess(characteristic =  "Is it a cat?"),
+//      no = AnimalGuess(characteristic = "Is the animal a dog?!?")
+//  )
+//
+//  println(newCharacteristic)
+//}
--- src/test/scala/AnimalGameSpec.scala
+++ src/test/scala/AnimalGameSpec.scala
@@ -1,7 +1,17 @@
-import org.junit.Test
-import org.junit.Assert.*
+import org.scalatest.freespec.AnyFreeSpec
 
-class Test1:
-  @Test def t1(): Unit = 
-    assertEquals("I was compiled by Scala 3. :)", "I was compiled by Scala 3. :)")
+class AnimalGameSpec extends AnyFreeSpec:
+  "The Animal Game" - {
+    "when I prepare a new game" - {
+      val animalGame = AnimalGame.build()
 
+      "animal game should be ready" in {
+        assert(animalGame.isReady() === true)
+      }
+
+//      "should have just one Characteristic Node" in {
+//
+//        assert(2 === 2)
+//      }
+    }
+  }
